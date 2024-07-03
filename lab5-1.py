cc441a623b9b52d111af22f666b0a8f4583ad6c3
jirayuchidcherd <6629011005@cdti.ac.th>
2024-07-03 16:17:15 +0700

    update

---
+++ lab5-1.py
@@ -0,0 +1,6 @@
+def rect(w, l):
+    area= w * l
+    return area
+w = int(input("กว้าง :"))
+l = int(input("ยาว :"))
+print ("พื้นที่สี่เหลี่ยม %d" % rect(w, l))
---
+++ lab5-3.py
@@ -0,0 +1,11 @@
+def f(c):
+    fx=(9/5)*(c+32)
+    return fx
+
+def k(c):
+    kx = c+273.15
+    return kx
+
+c = float(input("อุณหภูมิ =" ))
+print("F = %.2f ฟาเรนไฮส์" % f(c))
+print("K = %.2f เคลวิน" % k(c))
